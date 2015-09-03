4e4b1e79be7ed613d4c0f649c1f307a02b6b9e9a
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-03 13:09:12 -0400

    adds expand, register methods

--- index.js
+++ index.js
@@ -7,33 +7,94 @@
 
 'use strict';
 
-var App = require('template');
-var config = require('./template-config');
+var util = require('util');
+var forIn = require('for-in');
+var define = require('define-property');
+var toPath = require('to-object-path');
+var delegate = require('delegate-properties');
+var Task = require('expand-task');
+var Base = require('base-methods');
 var utils = require('./lib/utils');
 
-function Template(options) {
-  var app = new App(options);
-  app.mixin('config', config(app));
-  return app;
-}
+/**
+ * Create an instance of `Scaffold` with the given `options`.
+ *
+ * @param {Object} `options`
+ * @api public
+ */
 
 function Scaffold(options) {
-  this.options = options || {};
-  this.scaffolds = {};
+  if (!(this instanceof Scaffold)) {
+    return new Scaffold(options);
+  }
+  Base.call(this);
+  this.config = {};
 }
+Base.extend(Scaffold);
 
-utils.delegate(Scaffold.prototype, {
+/**
+ * Prototype methods
+ */
+
+delegate(Scaffold.prototype, {
   constructor: Scaffold,
 
-  register: function (name, options) {
-    this[name] = new Template(options);
+  /**
+   * Register a scaffold with the given `name` and optional `config`
+   * object.
+   *
+   * ```js
+   * scaffold.register('webapp', ...);
+   * ```
+   *
+   * @param  {String} `name`
+   * @param  {Object} `config`
+   * @return {Object}
+   * @api public
+   */
+
+  register: function (name, scaffold) {
+    var config = this.expand(name, scaffold);
+
+    forIn(config.targets, function (val, key) {
+      this.config[key] = val;
+    }, this);
+
+    return this;
+  },
+
+  expand: function (key, scaffold) {
+    if (typeof key === 'object') {
+      return new Task(this.setDefaults(key));
+    }
+    var config = {};
+    config[key] = this.setDefaults(scaffold);
+    return this.expand(config);
+  },
+
+  generate: function (name, data) {
+    generate(name, data);
+    return this;
+  },
+
+  setDefaults: function (scaffold) {
+    scaffold.options = merge({expand: true}, scaffold.options);
+    return scaffold;
+  },
+
+  option: function (key, value) {
+    this.set(toPath('options', key), value);
+    return this;
+  },
+
+  setItem: function (key, value) {
+    this.set(toPath('config', key), value);
     return this;
   }
 });
 
-
 /**
- * Expose `Scaffold`
+ * Expose `Scaffold`.
  */
 
 module.exports = Scaffold;
