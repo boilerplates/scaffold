19d3060bae3f3051dea74a17dfbc386b224a4c64
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-04 06:18:18 -0400

    100% coverage

--- test/test.js
+++ test/test.js
@@ -7,75 +7,61 @@ var Scaffold = require('../');
 var scaffold;
 
 describe('scaffold', function () {
-  beforeEach(function () {
-    scaffold = new Scaffold();
-  });
-
   describe('constructor:', function () {
+    it('should throw an error when name is missing:', function () {
+      (function () {
+        new Scaffold();
+      }).should.throw('expected name to be a string.');
+    });
+
+    it('should throw an error when config is not an object:', function () {
+      (function () {
+        new Scaffold('foo');
+      }).should.throw('expected config to be an object.');
+    });
+
+    it('should throw an error when properties are missing:', function () {
+      (function () {
+        new Scaffold('foo', {});
+      }).should.throw('scaffolds should have a src, dest or files property.');
+    });
+
     it('should return an instance of Scaffold:', function () {
+      scaffold = new Scaffold('foo', {src: 'foo'});
+      assert(scaffold instanceof Scaffold);
+    });
+
+    it('should return an instance of Scaffold without new:', function () {
+      scaffold = Scaffold('foo', {src: 'foo'});
       assert(scaffold instanceof Scaffold);
     });
   });
 
-  describe('static methods:', function () {
-    it('should expose `extend`:', function () {
-      assert(typeof Scaffold.extend === 'function');
-    });
-    it('should extend an object:', function () {
-      function foo() {}
-      Scaffold.extend(foo);
-      assert(typeof foo.extend === 'function');
-    });
-  });
-
-  describe('prototype methods:', function () {
-    it('should expose `visit`:', function () {
-      assert(typeof scaffold.visit === 'function');
-    });
-    it('should expose `get`:', function () {
-      assert(typeof scaffold.get === 'function');
-    });
-    it('should expose `set`:', function () {
-      assert(typeof scaffold.set === 'function');
-    });
-    it('should expose `register`:', function () {
-      assert(typeof scaffold.register === 'function');
-    });
-  });
-
-  describe('register:', function () {
-    it('should add a scaffold to the targets object:', function () {
-      scaffold.register('header', {
+  describe('instance:', function () {
+    it('should create a scaffold:', function () {
+      var scaffold = new Scaffold('header', {
         cwd: 'test/templates',
-        src: ['[a-c].txt']
+        src: '[a-c].txt'
       });
-      assert(typeof scaffold.targets.header === 'object');
+
+      assert(typeof scaffold.options === 'object');
+      assert(Array.isArray(scaffold.files));
+      assert(scaffold.files.length > 0);
     });
 
     it('should support passing `src` as a string:', function () {
-      scaffold.register('header', 'test/templates/[a-c].txt');
-      assert(typeof scaffold.targets.header === 'object');
+      var scaffold = new Scaffold('header', 'test/templates/[a-c].txt');
+      assert(scaffold.files.length > 0);
     });
 
     it('should support passing `src` as an array:', function () {
-      scaffold.register('header', ['test/templates/[a-c].txt']);
-      assert(typeof scaffold.targets.header === 'object');
+      var scaffold = new Scaffold('header', ['test/templates/[a-c].txt']);
+      assert(scaffold.files.length > 0);
     });
 
     it('should support passing options as the third argument:', function () {
-      scaffold.register('header', '[a-c].txt', {cwd: 'test/templates'});
-      assert(typeof scaffold.targets.header === 'object');
-    });
-
-    it('should be chainable:', function () {
-      var scaffold = new Scaffold({cwd: 'test/templates'})
-        .register('header', '[a-b].txt')
-        .register('footer', '[c-d].txt')
-        .register('sidebar', '[e-f].txt')
-
-      assert(typeof scaffold.targets.header === 'object');
-      assert(typeof scaffold.targets.footer === 'object');
-      assert(typeof scaffold.targets.sidebar === 'object');
+      var scaffold = new Scaffold('header', '[a-c].txt', {cwd: 'test/templates'});
+      assert(scaffold.files.length > 0);
     });
   });
 });
