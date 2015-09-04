afdc8d0d1a7be268f6b79ea9268c63f923e51884
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-04 06:19:08 -0400

    simplify

--- index.js
+++ index.js
@@ -7,95 +7,75 @@
 
 'use strict';
 
-var Base = require('base-methods');
-var toPath = require('to-object-path');
 var Target = require('expand-target');
 var merge = require('mixin-deep');
 
 /**
- * Create an instance of Scaffold with the
- * given `options`
+ * Create a new Scaffold with the given `name` and `config`.
  *
  * ```js
- * var scaffold = new Scaffold();
+ * var component = new Scaffold('component', {
+ *   src: ['~/templates/*.js']
+ * });
  * ```
- * @param {Object} `options`
+ * @param {Object} `name`
  * @api public
  */
 
-function Scaffold(options) {
+function Scaffold(name, config, options) {
   if (!(this instanceof Scaffold)) {
-    return new Scaffold(options);
+    return new Scaffold(name, config, options);
   }
-  Base.call(this);
-  this.options = options || {};
-  this.targets = {};
-}
-Base.extend(Scaffold);
+  if (typeof name !== 'string') {
+    throw new TypeError('expected name to be a string.');
+  }
 
-/**
- * Register a scaffold "target" with the given `name`. A
- * target is a semantically-grouped configuration of
- * files and directories.
- *
- * ```js
- * scaffold.register('webapp', ...);
- * ```
- *
- * @param  {String} `name` The name of the config target.
- * @param  {Object} `config`
- * @return {Object}
- * @api public
- */
+  config = this.create(config, options);
 
-Scaffold.prototype.register = function(name, config, options) {
-  this.targets[name] = new Target(name, this.defaults(config, options));
-  return this;
-};
-
-/**
- * Set or get an option to be used as a default value
- * when registering scaffold targets. Pass a key-value
- * pair or an object to set a value, or the key of
- * the value to get.
- *
- * ```js
- * scaffold.option('cwd', 'templates/');
- * ``
- * @param {String|Object|Array} `key`
- * @param {any} `value`
- * @return {Object} Returns the instance of `Scaffold` for chaining
- * @api public
- */
-
-Scaffold.prototype.option = function(key, value) {
-  if (typeof key === 'string') {
-    key = toPath('options', key);
-    if (arguments.length === 1) {
-      return this.get(key, value);
+  var scaffold = new Target(name, config);
+  for (var key in scaffold) {
+    if (scaffold.hasOwnProperty(key)) {
+      this[key] = scaffold[key];
     }
-  } else {
-    this.visit('option', key);
-    return this;
   }
-  this.set(key, value);
-  return this;
-};
+}
 
 /**
  * Set default values on targets.
  */
 
-Scaffold.prototype.init = function(config, options) {
+Scaffold.prototype.create = function(config, options) {
+  if (typeof config === 'undefined') {
+    throw new TypeError('expected config to be an object.');
+  }
   if (typeof config === 'string' || Array.isArray(config)) {
     config = { src: config };
   }
   if (typeof options === 'object') {
-    config.options = options;
+    config.options = merge({}, options, config.options);
   }
-  config.options = merge({expand: true}, this.options, config.options);
+  if (!hasAny(config, ['src', 'dest', 'files'])) {
+    throw new Error('scaffolds should have a src, dest or files property.');
+  }
+
+  config.options = merge({expand: true}, config.options);
   return config;
 };
+
+/**
+ * Return `true` if an object has any of the given keys.
+ *
+ * @param {Object} `obj`
+ * @param {Array} `keys`
+ * @return {Boolean}
+ */
+
+function hasAny(obj, keys) {
+  for (var key in obj) {
+    if (keys.indexOf(key) > -1) return true;
+  }
+  return false;
+}
 
 /**
  * Expose `Scaffold`
