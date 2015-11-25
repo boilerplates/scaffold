214356c38b13e05298cd8c7a31ffc0691ea72da9
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-25 09:22:54 -0500

    adds tests, fixtures

--- test/test.js
+++ test/test.js
@@ -1,71 +1,30 @@
 'use strict';
 
-/* deps: mocha */
+require('mocha');
+require('should');
+var util = require('util');
 var assert = require('assert');
-var should = require('should');
-var Scaffold = require('../');
+var Scaffold = require('..');
 var scaffold;
 
-describe('scaffold', function () {
-  describe('constructor:', function () {
-    it('should throw an error when config is not an object:', function () {
-      (function () {
-        new Scaffold();
-      }).should.throw('expected config to be an object.');
-    });
-
-    it('should throw an error when properties are missing:', function () {
-      (function () {
-        new Scaffold({});
-      }).should.throw('scaffolds should have a src, dest or files property.');
-    });
-
-    it('should return an instance of Scaffold:', function () {
-      scaffold = new Scaffold('foo', {src: 'foo'});
-      assert(scaffold instanceof Scaffold);
-    });
-
-    it('should return an instance of Scaffold without new:', function () {
-      scaffold = Scaffold('foo', {src: 'foo'});
-      assert(scaffold instanceof Scaffold);
-    });
+describe('scaffolds', function () {
+  beforeEach(function() {
+    scaffold = new Scaffold();
   });
 
-  describe('instance:', function () {
-    it('should create a scaffold:', function () {
-      var scaffold = new Scaffold({
-        cwd: 'test/templates',
-        src: '[a-c].txt'
+  describe('targets', function () {
+    it('should expose an "options" property', function () {
+      scaffold.addTargets({});
+      assert(scaffold.options);
+    });
+
+    it('should expose targets', function () {
+      scaffold.addTargets({
+        foo: {src: '*'},
+        bar: {src: '*'}
       });
-
-      assert(typeof scaffold.options === 'object');
-      assert(Array.isArray(scaffold.files));
-      assert(scaffold.files.length > 0);
-    });
-
-    it('should support passing `src` as a string:', function () {
-      var scaffold = new Scaffold('test/templates/[a-c].txt');
-      assert(scaffold.files.length > 0);
-    });
-
-    it('should support passing `src` as an array:', function () {
-      var scaffold = new Scaffold(['test/templates/[a-c].txt']);
-      assert(scaffold.files.length > 0);
-    });
-
-    it('should support passing options as the third argument:', function () {
-      var scaffold = new Scaffold('[a-c].txt', {cwd: 'test/templates'});
-      assert(scaffold.files.length > 0);
-    });
-
-  });
-  describe('options.process:', function () {
-    it('should process config templates:', function () {
-      var scaffold = new Scaffold('<%= foo %>', {
-        process: true,
-        foo: 'bar'
-      });
-      assert(scaffold.files[0].src[0] === 'bar');
+      assert(scaffold.foo);
+      assert(scaffold.bar);
     });
   });
 });
